5d5558f6b12626f5d20d3a7acaac61f92d486859
Nth <praisemomoh2kwbook@gmail.com>
2024-11-26 22:25:42 +0100

    initial commit

---
+++ scripts.js
@@ -0,0 +1,30 @@
+// Optional: Adds scrolling control between sections
+// const sections = document.querySelectorAll('section');
+// let currentSectionIndex = 0;
+
+// function scrollToSection(index) {
+//   sections[index].scrollIntoView({ behavior: 'smooth' });
+// }
+
+// window.addEventListener('wheel', (event) => {
+//   if (event.deltaY > 0) {
+//     // Scrolling down
+//     if (currentSectionIndex < sections.length - 1) {
+//       currentSectionIndex++;
+//       scrollToSection(currentSectionIndex);
+//     }
+//   } else {
+//     // Scrolling up
+//     if (currentSectionIndex > 0) {
+//       currentSectionIndex--;
+//       scrollToSection(currentSectionIndex);
+//     }
+//   }
+// });
+
+// JavaScript for Hamburger Menu
+function showSideNav() {
+    const sidenav = document.querySelector(".side-nav");
+    sidenav.classList.toggle("visible");
+  }
+  
